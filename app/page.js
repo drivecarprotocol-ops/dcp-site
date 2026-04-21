a6394f63b35293b69afed3cf92ce783c3bc2f70a
drivecarprotocol-ops <drivecarprotocol@gmail.com>
2026-04-21 02:30:46 -0400

    mobile

--- app/page.js
+++ app/page.js
@@ -127,7 +127,7 @@ export default function DCPWebsiteV2() {
     Event → Emotion → Reaction
   </div>
 
-  <div className="-mt-3 ml-[78px] text-blue-300 text-xl leading-none">
+  <div className="-mt-2 ml-[80px] text-blue-300 text-xl leading-none">
     ↓
   </div>
 
