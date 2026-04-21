6ef672c245d80955e00f8309a48ae88e1cc6663c
drivecarprotocol-ops <drivecarprotocol@gmail.com>
2026-04-20 22:21:24 -0400

    placement

--- app/page.js
+++ app/page.js
@@ -65,7 +65,7 @@ export default function DCPWebsiteV2() {
                 <img
                   src="/images/icons/cone.png"
                   alt=""
-                  className="pointer-events-none absolute left-[90%] top-[10px] z-20 w-16 opacity-95 md:left-[92%] md:top-[14px] md:w-20"
+                  className="pointer-events-none absolute left-[90%] top-[10px] z-20 w-16 opacity-95 md:left-[98%] md:top-[14px] md:w-20"
                 />
               </div>
 
