df9eb955108dbcfbf3294910728fec5a77086adc
drivecarprotocol-ops <drivecarprotocol@gmail.com>
2026-04-24 21:29:07 -0400

    -80

--- app/page.js
+++ app/page.js
@@ -208,7 +208,7 @@ export default function DCPWebsiteV2() {
           style={{
             backgroundImage: "url('/images/backgrounds/lit-cones-mobile.jpg')",
             backgroundSize: "85% auto",
-            backgroundPosition: "center calc(100% - 40px)",
+            backgroundPosition: "center calc(100% - 80px)",
           }}
         />
         <div className="absolute inset-0 bg-black/62" />
